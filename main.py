dd65d9edbd9e39383cd4dbc1b5396c7fcf8fa9ce
Art <art.mtsh@gmail.com>
2025-02-17 20:38:33 +0200

    Total update

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
 from dotenv import load_dotenv
-
 load_dotenv('main.env')
 
-from new_version.main_log_config import setup_logger
+from main_log_config import setup_logger
 setup_logger(os.path.dirname(__file__))
 
 import asyncio
@@ -15,15 +14,19 @@ from new_version.bot_setup.bot_sender import update_message_every_5_seconds
 async def main():
 
     # Start the background update task
-    update_task = asyncio.create_task(update_message_every_5_seconds())
-    await asyncio.sleep(30)
+
+    tasks = [asyncio.create_task(update_message_every_5_seconds())]
 
     # Start trading tasks
     excluded = ['BTCUSDT', 'ETHUSDT']
     live_coins = await get_pairs(excluded, 'USDT', 0.05, 0.3, 60)
-    tasks = [asyncio.create_task(search(coin)) for coin in live_coins]
 
-    await asyncio.gather(*tasks, update_task)
+    await asyncio.sleep(30)
+
+    for coin in live_coins:
+        tasks.append(asyncio.create_task(search(coin)))
+
+    await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
