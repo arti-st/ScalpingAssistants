4088642b6dba1e94c60217ec729a916cc8ff5d41
Art <art.mtsh@gmail.com>
2025-02-17 21:29:05 +0200

    Total update

--- new_version/search_for_levels.py
+++ new_version/search_for_levels.py
@@ -14,6 +14,7 @@ async def search(coin, update_lock):
 
         wiggle_room_perc = 0.003
         atr_dis = 4.0  # мультиплікатор відстані до сайзу в ATR
+        abs_dis = 0.5  # мультиплікатор відстані до сайзу %
         size_mpl = 1.3  # мультиплікатор максимального сайзу
         vol_mpl = 3.0 # мультиплікатор відносності об'єму
 
@@ -35,7 +36,12 @@ async def search(coin, update_lock):
 
                 # пошук екстремуму, а потім сайзу на ньому
                 for i in range(2, len(c_low) - c_room):
-                    if c_high[-i] >= max(c_high[-1: -i - c_room: -1]):
+
+                    # дистанція до ціни
+                    distance_per = abs(c_high[-i] - c_close[-1]) / (c_close[-1] / 100)
+                    distance_per = float('{:.2f}'.format(distance_per))
+
+                    if c_high[-i] >= max(c_high[-1: -i - c_room: -1]) and distance_per <= atr_dis * avg_atr_per and distance_per <= abs_dis:
                         for item in depth:
                             wiggle_high = c_high[-i] * (1 + wiggle_room_perc)
                             wiggle_low = c_high[-i] * (1 - wiggle_room_perc)
@@ -45,11 +51,8 @@ async def search(coin, update_lock):
                                 lower_sizes = [depth[k][1] for k in range(depth.index(item) - d_room, depth.index(item))]
                                 # сайзи між ціною щільності +10 та ціною щільності
                                 higher_sizes = [depth[k][1] for k in range(depth.index(item) + 1, depth.index(item) + d_room + 1)]
-                                # дистанція до ціни
-                                distance_per = abs(c_high[-i] - c_close[-1]) / (c_close[-1] / 100)
-                                distance_per = float('{:.2f}'.format(distance_per))
 
-                                if all(item[1] >= dom * size_mpl for dom in lower_sizes + higher_sizes) and distance_per <= atr_dis * avg_atr_per and item[1] >= avg_vol * vol_mpl:
+                                if all(item[1] >= dom * size_mpl for dom in lower_sizes + higher_sizes) and item[1] >= avg_vol * vol_mpl:
                                     print(f'{coin} {market_type_verbose} found high extr: {c_high[-i]}!')
                                     direction = '🔼' if item[0] >= c_close[-1] else '🔽'
                                     result[round(distance_per, 2)] = (
@@ -62,22 +65,22 @@ async def search(coin, update_lock):
                                     )
                                 # break
 
-                    if c_low[-i] <= min(c_low[-1: -i - c_room: -1]):
+                    # дистанція до ціни
+                    distance_per = abs(c_low[-i] - c_close[-1]) / (c_close[-1] / 100)
+                    distance_per = float('{:.2f}'.format(distance_per))
+
+                    if c_low[-i] <= min(c_low[-1: -i - c_room: -1]) and distance_per <= atr_dis * avg_atr_per and distance_per <= abs_dis:
                         for item in depth:
                             wiggle_high = c_low[-i] * (1 + wiggle_room_perc)
                             wiggle_low = c_low[-i] * (1 - wiggle_room_perc)
                             # щільність знаходиться між 9-ю спочатку, 9-ю з кінця та ціна щільності == лою
                             if d_room - 1 < depth.index(item) < len(depth) - d_room and wiggle_low <= item[0] <= wiggle_high:
-
                                 # сайзи між ціною щільності -10 та ціною щільності
                                 lower_sizes = [depth[k][1] for k in range(depth.index(item) - d_room, depth.index(item))]
                                 # сайзи між ціною щільності +10 та ціною щільності
                                 higher_sizes = [depth[k][1] for k in range(depth.index(item) + 1, depth.index(item) + d_room + 1)]
-                                # дистанція до ціни
-                                distance_per = abs(c_low[-i] - c_close[-1]) / (c_close[-1] / 100)
-                                distance_per = float('{:.2f}'.format(distance_per))
 
-                                if all(item[1] >= dom * size_mpl for dom in lower_sizes + higher_sizes) and distance_per <= atr_dis * avg_atr_per and item[1] >= avg_vol * vol_mpl:
+                                if all(item[1] >= dom * size_mpl for dom in lower_sizes + higher_sizes) and item[1] >= avg_vol * vol_mpl:
                                     print(f'{coin} {market_type_verbose} found low extr: {c_low[-i]}!')
                                     direction = '🔼' if item[0] >= c_close[-1] else '🔽'
                                     result[round(distance_per, 2)] = (
